9c8614c1f0c7ee15f514a3886eedae1e56c85ff1
L7z404 <leonelcastillo1014@gmail.com>
2021-10-16 18:31:17 -0500

    Archivos hasta el video 14

---
+++ forms.py
@@ -0,0 +1,21 @@
+from wtforms import Form
+from wtforms import StringField, TextField
+from wtforms.fields.html5 import EmailField
+from wtforms import HiddenField
+from wtforms import validators
+
+def length_honeypot(form, field): 
+    if len(field.data) > 0: 
+        raise validators.ValidationError('El campo debe estar vacío.')
+
+class CommentForm(Form):
+    username = StringField('Nombre de Usuario', [
+        validators.Required(message='El nombre de usuario es requerido'),
+        validators.length(min=4, max=25, message='Ingrese un nombre de usuario válido')
+    ])
+    email = EmailField('Correo Electronico', [
+        validators.Required(message='El email es requerido'),
+        validators.Email(message='Ingrese un email válido')
+    ])
+    comment = TextField('Comentario')
+    honeypot = HiddenField('', [length_honeypot])
