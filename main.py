77b8f079d60938bbe2232ff32e4215877a7e8772
L7z404 <leonelcastillo1014@gmail.com>
2021-10-17 16:15:28 -0500

    hasta vid 20

--- main.py
+++ main.py
@@ -2,27 +2,49 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import make_response
-from flask_wtf import CsrfProtect
+from flask import session
+from flask import flash
+from flask.helpers import url_for
+from flask import redirect
+from flask_wtf import CSRFProtect
+from werkzeug.utils import redirect
 
 import forms
+import json
 
 app = Flask(__name__)
 app.secret_key = 'secret'
-csrf = CsrfProtect(app)
+csrf = CSRFProtect(app)
+
+@app.errorhandler(404)
+def page_not_found(e): 
+    return render_template('404.html'), 404
 
 @app.route('/')
 def index(): 
-    custom_cookie = request.cookies.get('custom_cookie', 'Undefined')
-    print(custom_cookie)
+    # custom_cookie = request.cookies.get('custom_cookie', 'Undefined')
+    # print(custom_cookie)
+    if 'username' in session: 
+        username = session['username']
+        print(username)
     title = 'Index'
     return render_template('index.html', title=title)
+
+@app.route('/logout')
+def logout(): 
+    if 'username' in session: 
+        session.pop('username') 
+    return redirect(url_for('login'))
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login(): 
     login_form = forms.LoginForm(request.form)
     if request.method == 'POST' and login_form.validate(): 
-        print(login_form.username.data)
-        print(login_form.password.data)
+        username = login_form.username.data
+        success_message = 'Bienvenido {}'.format(username)
+        flash(success_message)
+        session['username'] = login_form.username.data
+        
     title = 'Login'
     return render_template('login.html', title=title, form = login_form)
 
@@ -44,6 +66,13 @@ def cookie():
     response = make_response(render_template('cookie.html'))
     response.set_cookie('custom_cookie', 'La cookie de Leonel')
     return response
+
+@app.route('/ajax-login', methods = ['POST'])
+def ajax_login(): 
+    print(request.form)
+    username = request.form['username']
+    response = {'status':200, 'username':username, 'id':1}
+    return json.dumps(response)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
-    console.log("Hola Mundo")
+    console.log("Mensaje en la consola que indica que todo esta bien.")
+    
+    function ajax_login() {
+        $.ajax({
+            url: '/ajax-login',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
+    
+    $("#login-form").submit(function (event) {
+        event.preventDefault();
+        ajax_login();
+    })
 });
